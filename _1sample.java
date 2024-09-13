f834d7c3896e90afcfe905478b9bd691c39886a3
PriyanshiSharma26 <priyanshisharma8220708@gmail.com>
2024-09-13 10:00:39 -0700

    Add files via upload

---
+++ _1sample.java
@@ -0,0 +1,12 @@
+class first
+{
+    public static void main(String[] args)
+    
+     {
+        int a=10;
+        int b=30;
+        int c=a+b;
+        System.out.println("sum is: "+c);
+        System.out.println("my name is priyanshi");
+    }
+}
