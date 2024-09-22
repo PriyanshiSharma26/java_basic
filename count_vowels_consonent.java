2480d3ab9b9f1d25d917977b3f52d96435c27b58
PriyanshiSharma26 <priyanshisharma8220708@gmail.com>
2024-09-22 09:31:19 -0700

    Add files via upload

---
+++ count_vowels_consonent.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+ class CountVowelsConsonants {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine().toLowerCase();
+        int vowels = 0, consonants = 0;
+
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if ("aeiou".indexOf(c) != -1) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
+
+
+//other way
+ class CountVowelsConsonants2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        int vowels = 0, consonants = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            } else if (c >= 'A' && c <= 'Z') {
+                if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
+
+ 
